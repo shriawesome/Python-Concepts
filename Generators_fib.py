d83dba78f5031b2f49e470c4cc8794ec3c1bfcfd
Shri <siddd.shriii@gmail.com>
2020-12-30 16:27:20 +0530

    Understanding Generators

--- Generators_fib.py
+++ Generators_fib.py
@@ -24,5 +24,20 @@ for i in fib(5):
     print(i)
 
 
-# Generators come handy when we don't want the entire values in a single go in the memory,
-# and use one value at a time which can save the memory consumption.
+# BENEFIT of using a GENERATORS : 
+# 1. makes the code more readable.
+# 2. Generators come handy when we don't want the entire values in a single go in the memory,
+#    and use one value at a time which can save the memory consumption.
+
+# When all the 'next()' options are exhausted then it returns an "StopIteration" error.
+# 'for' loop can be used in place of using 'next()' all the time.
+
+'''
+# Squaring the numbers using list comprehension to yield result in generator
+
+# By using '(',')' one can yeild result in generators, in below sqr_nums is a generator
+sqr_nums=(x*x for x in [1,2,3,4,5])
+
+# To print all the numbers together, by doing this we loose the performance benefits of using a generator
+print(list(sqr_nums))
+'''
